1836ab9022d7e1552244ad1bf6a32592e4d32326
Junaid Usmani <junaid22usmani@gmail.com>
2026-01-11 08:16:40 +0530

    day 3 done

---
+++ day-3.js
@@ -0,0 +1,51 @@
+/* -------------------------------
+PROBLEM 1: ARRAY SUM
+---------------------------------
+
+Given an array of numbers:
+
+const nums = [5, 10, 15, 20];
+
+TASK:
+1. Find the sum of all numbers
+2. Store the result in a variable called `totalSum`
+3. Log the result
+
+Expected Output:
+50
+
+*/
+
+// 👉 Write your solution below 👇
+const nums = [5, 10, 15, 20];
+let totalSum = 0;
+
+for (let num of nums) {
+    totalSum = totalSum + num;
+}
+
+console.log(totalSum);
+
+/* -------------------------------
+PROBLEM 2: STRING FIRST CHARACTER
+---------------------------------
+
+Given a string:
+
+const text = "chaos";
+
+TASK:
+1. Get the first character of the string
+2. Store it in a variable called `firstChar`
+3. Log the result
+
+Expected Output:
+"c"
+
+*/
+
+// 👉 Write your solution below 👇
+const text = "chaos";
+let firstChar = text[0];
+
+console.log(firstChar);
