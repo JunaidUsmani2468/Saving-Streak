3b3050e27d47ae92f2257d3849cb9180d6446835
Junaid Usmani <junaid22usmani@gmail.com>
2026-01-16 10:58:44 +0530

    day-8 complete (5min)

---
+++ day-8.js
@@ -0,0 +1,57 @@
+/*
+--------------------------------
+PROBLEM 1: ARRAY ZERO COUNT
+--------------------------------
+
+Given an array of numbers:
+*/
+
+const nums = [0, 3, 0, 5, 7, 0, 1];
+
+/*
+TASK:
+1. Count how many numbers are ZERO
+2. Store the count in a variable called `zeroCount`
+3. Log the result
+
+Expected Output:
+3
+*/
+
+// 👉 Write your solution below 👇
+
+let zeroCount = 0;
+
+for (let num of nums) {
+    if (num === 0) {
+        zeroCount++;
+    }
+}
+
+console.log(zeroCount);
+
+/*
+--------------------------------
+PROBLEM 2: STRING CHARACTER COUNT
+--------------------------------
+
+Given a string:
+*/
+
+const text = "consistency";
+
+/*
+TASK:
+1. Count how many characters are in the string
+2. Store the count in a variable called `charCount`
+3. Log the result
+
+Expected Output:
+11
+*/
+
+// 👉 Write your solution below 👇
+
+let charCount = text.length;
+
+console.log(charCount);
