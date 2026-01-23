157b3434daeb57c99c1a7f0d4ee365624231ba45
Junaid Usmani <junaid22usmani@gmail.com>
2026-01-23 10:30:39 +0530

    day15 complete

---
+++ week-3/day-15.js
@@ -0,0 +1,41 @@
+/*
+--------------------------------
+PROBLEM: STRING UNIQUE CHARACTER COUNT
+--------------------------------
+
+Given a string:
+*/
+
+let text = "developer";
+
+/*
+TASK:
+1. Count how many UNIQUE characters are in the string
+   (case-sensitive)
+2. Store the count in a variable called `uniqueCount`
+3. Log the result
+
+Expected Output: 7
+(Explanation: d, e, v, l, o, p, r → but 'e' repeats)
+*/
+
+// 👉 Write your solution below 👇
+
+let uniqueArr = [];
+
+for (let char of text) {
+    let isUnique = true;
+
+    for (let i = 0; i < uniqueArr.length; i++) {
+        if (char === uniqueArr[i]) {
+            isUnique = false;
+            break;
+        } 
+    }
+
+    if (isUnique) {
+        uniqueArr.push(char);
+    }
+}
+
+console.log(uniqueArr.length);
